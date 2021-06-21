af92cccdc9a14d6acb07c4a9302cbdc21eda7b8e
eutro <benedek.szilvasy@gmail.com>
2021-06-21 19:08:31 +0100

    Inline flattener dependency into the jar.

--- hierarchical-lang-gradle/build.gradle.kts
+++ hierarchical-lang-gradle/build.gradle.kts
@@ -1,16 +1,11 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
-import groovy.namespace.QName
-import groovy.util.Node
-
 plugins {
     id("com.gradle.plugin-publish") version "0.15.0"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
     `java-gradle-plugin`
     `maven-publish`
 }
 
 base {
-    version = "1.1.2"
+    version = "1.1.3"
 }
 
 repositories {
@@ -18,38 +13,12 @@ repositories {
 }
 
 dependencies {
-    shadow(gradleApi())
-    shadow(localGroovy())
-
     implementation("com.google.code.gson:gson:2.8.7")
-    implementation(project(":flattener"))
-}
-
-tasks.register<ConfigureShadowRelocation>("relocateShadowJar") {
-    prefix = "eutro.hierarchicallang.shadow"
-    target = tasks.shadowJar.get()
+    compileOnly(project(":flattener"))
 }
 
 tasks.jar {
-    enabled = false
-}
-
-tasks.shadowJar {
-    dependsOn("relocateShadowJar")
-    archiveClassifier.set("")
-}
-
-tasks.build {
-    dependsOn(tasks.shadowJar)
-}
-
-// needed to prevent inclusion of gradle-api into shadow JAR
-configurations {
-    implementation {
-        dependencies {
-            project.dependencies.gradleApi()
-        }
-    }
+    from(project(":flattener").tasks.compileJava.get().outputs)
 }
 
 gradlePlugin {
@@ -71,41 +40,4 @@ pluginBundle {
         }
     }
     tags = listOf("json", "transform")
-}
-
-// Workaround for configuring the artifact that publish-plugins uses: https://github.com/JLLeitschuh/ktlint-gradle/blob/master/plugin/build.gradle.kts
-// Need to move publishing configuration into afterEvaluate {}
-// to override changes done by "com.gradle.plugin-publish" plugin in afterEvaluate {} block
-// See PublishPlugin class for details
-afterEvaluate {
-    publishing {
-        publications {
-            withType<MavenPublication> {
-                // Special workaround to publish shadow jar instead of normal one. Name to override peeked here:
-                // https://github.com/gradle/gradle/blob/master/subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/MavenPluginPublishPlugin.java#L73
-                if (name == "pluginMaven") {
-                    setArtifacts(
-                        listOf(
-                            tasks.shadowJar,
-                            tasks.sourcesJar,
-                            tasks["publishPluginJavaDocsJar"]
-                        )
-                    )
-                }
-                pom {
-                    withXml {
-                        asNode().run {
-                            for (child in children()) {
-                                if (child is Node && child.name().let {
-                                        it is QName && it.localPart == "dependencies"
-                                    }) {
-                                    child.children().clear()
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
